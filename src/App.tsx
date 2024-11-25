0031870f28d28a0208e9cbbdaeab14920f43965c
Abdul Arif <abdul.arif0705@gmail.com>
2024-11-25 12:25:17 -0500

    jsonforms example

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { JsonForms } from "@jsonforms/react";
+import logo from "./logo.svg";
+import "./App.css";
+import { person } from "@jsonforms/examples";
+import {
+  materialRenderers,
+  materialCells,
+} from "@jsonforms/material-renderers";
+
+const schema = person.schema;
+const uischema = person.uischema;
+const initialData = person.data;
 
 function App() {
+  const [data, setData] = useState(initialData);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <JsonForms
+        schema={schema}
+        uischema={uischema}
+        data={data}
+        renderers={materialRenderers}
+        cells={materialCells}
+        onChange={({ data, errors }) => setData(data)}
+      />
     </div>
   );
 }
